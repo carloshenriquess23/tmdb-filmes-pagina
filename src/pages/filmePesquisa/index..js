36f623e1c0e7eae75018ab0945c355ed0e855fed
carloshenriquess23 <scarloshenrique213@gmail.com>
2024-06-16 21:19:39 -0300

    finalização da pagina home, inciio da pagina pesquisa

---
+++ src/pages/filmePesquisa/index..js
@@ -0,0 +1,11 @@
+import './index.scss';
+
+export default function FilmePesquisaPage(){
+
+    return(
+
+        <div className='pagina-pesquisa'>
+            
+        </div>
+    );
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,12 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import FilmeHomePage from '../pages/filmesHome';
+import FilmePesquisaPage from "../pages/filmePesquisa/index.";
 
 const Rotas=()=>(
     <BrowserRouter>
         <Routes>
-            <Route path='/filmes' element={<FilmeHomePage/>}></Route>
+            <Route path='/' element={<FilmeHomePage/>}></Route>
+            <Route path='/filme/buscar/:nome'></Route>
         </Routes>
     </BrowserRouter>
 )

48134843130f2f437442866e17549607d1f1bf86
carloshenriquess23 <scarloshenrique213@gmail.com>
2024-06-20 15:05:31 -0300

    url portfolio

--- src/components/cabecalho/index.js
+++ src/components/cabecalho/index.js
@@ -6,7 +6,7 @@ export default function Header(){
 
         <header className='cabecalho-page'>
             <p>Projeto para meu portfolio.</p>
-            <a href="https://portfolio-4jwhve5uz-carlos-projects-6c33deb2.vercel.app">Ver Portfolio</a>
+            <a href="https://portfolio-page-7t665yre3-carlos-projects-6c33deb2.vercel.app">Ver Portfolio</a>
         </header>
     );
 }
